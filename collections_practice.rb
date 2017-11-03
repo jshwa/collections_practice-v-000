fddaceddd092a9666dae01f845b6c70e571b5621
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-03 17:14:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1] <=> array[2]
+end
